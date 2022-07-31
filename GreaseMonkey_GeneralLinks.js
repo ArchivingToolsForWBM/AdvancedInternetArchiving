b3fa62209c344cda17842f938e2738ec1b33f9a6
GhbSmwc <89951208+ArchivingToolsForWBM@users.noreply.github.com>
2022-07-31 16:30:38 -0400

    Added a general greasemonkey script to extract links

---
+++ GreaseMonkey_GeneralLinks.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Extract any links from any site
+// @namespace    any site
+// @version      0.2
+// @description  try to take over the world!
+// @include      *
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    const all = window.allLink = new Set();
+    
+    function getLink() {
+        Array.from(document.querySelectorAll('[href]')).forEach(link=>{ //"a href" links
+            if(!all.has(link.href)) {
+                all.add(link.href);
+                console.log((link.href).replace(/^http/, "ttp").replace(/#.*$/, ""));
+            }
+        });
+        Array.from(document.querySelectorAll('[src]')).forEach(link=>{ //Image
+            if(!all.has(link.src)) {
+                all.add(link.src);
+                console.log((link.src).replace(/^http/, "ttp").replace(/#.*$/, ""));
+            }
+        });
+        Array.from(document.querySelectorAll('[style]')).forEach(link=>{ //Background images
+            if(!all.has(link.style)) {
+                if ((link.style.backgroundImage).replace(/url\((\"|\')/, "").replace(/(\"|\')\)/, "").replace(/^http/, "ttp") != "") {
+                    all.add(link.style);
+                    console.log((link.style.backgroundImage).replace(/url\((\"|\')/, "").replace(/(\"|\')\)/, "").replace(/^http/, "ttp"));
+                }
+            }
+        });
+    }
+    getLink();
+    window.addEventListener('scroll',getLink);
+})();
