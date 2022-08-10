be6e679cb28cfc16890074a75dcd9bdbf75fb727
GhbSmwc <89951208+ArchivingToolsForWBM@users.noreply.github.com>
2022-08-10 19:00:51 -0400

    some fixes
    While querySelectorAll is great, it has a major limitation that this fails on certain javascript-heavy sites like emotionflow(dot)com.

--- GreaseMonkey_GeneralLinks.js
+++ GreaseMonkey_GeneralLinks.js
@@ -13,19 +13,19 @@
     const all = window.allLink = new Set();
     
     function getLink() {
-        Array.from(document.querySelectorAll('[href]')).forEach(link=>{ //"a href" links
+        Array.from(document.getElementsByTagName('a')).forEach(link=>{ //"a href" links
             if(!all.has(link.href)) {
                 all.add(link.href);
                 console.log((link.href).replace(/^http/, "ttp").replace(/#.*$/, ""));
             }
         });
-        Array.from(document.querySelectorAll('[src]')).forEach(link=>{ //Image
+        Array.from(document.getElementsByTagName('img')).forEach(link=>{ //Images
             if(!all.has(link.src)) {
                 all.add(link.src);
                 console.log((link.src).replace(/^http/, "ttp").replace(/#.*$/, ""));
             }
         });
-        Array.from(document.querySelectorAll('[style]')).forEach(link=>{ //Background images
+        Array.from(document.getElementsByTagName('*')).forEach(link=>{ //Background images
             if(!all.has(link.style)) {
                 if ((link.style.backgroundImage).replace(/url\((\"|\')/, "").replace(/(\"|\')\)/, "").replace(/^http/, "ttp") != "") {
                     all.add(link.style);
