20d9595ef437af9576ab9e0cd02087230710ac61
GhbSmwc <89951208+ArchivingToolsForWBM@users.noreply.github.com>
2022-08-12 22:30:30 -0400

    some fixes
    The extraction of links in the background-image did not work on one website, now it is fixed.

--- GreaseMonkey_GeneralLinks.js
+++ GreaseMonkey_GeneralLinks.js
@@ -26,26 +26,27 @@
             }
         });
         Array.from(PageDocument.getElementsByTagName('*')).forEach(link=>{ //Background images
-            if(!all.has(link.style)) {
-                if ((link.style.backgroundImage).replace(/url\((\"|\')/, "").replace(/(\"|\')\)/, "").replace(/^http/, "ttp") != "") {
+            if(!all.has(link.style.backgroundImage)&&link.style.backgroundImage!="") {
                     all.add(link.style);
                     console.log((link.style.backgroundImage).replace(/url\((\"|\')/, "").replace(/(\"|\')\)/, "").replace(/^http/, "ttp"));
-                }
             }
         });
     }
     //Code that executes when the MAIN WINDOW loads the page
     //Please note that this does not reflect the loading of subwindows when you open links in a way that does not reload the main window
     //Since this executes ONCE when the main window loads.
-    {
+  
+    window.addEventListener('load',getLink.bind(null, document)); //Get links on the main window when page finishes loading
+    window.addEventListener('load', (event) => {
         let CurrentDocument = document
-        window.addEventListener('scroll',getLink.bind(null, CurrentDocument)); //Get links on the main window when loaded
+        window.addEventListener('scroll',getLink.bind(null, CurrentDocument)); //Get links on the main window when scrolling (when page loads as you scroll; infinute scroll)
         if (window.frames.length) { //Loop through every window and extract their links too (NOTE: will not extract recursively)
             for (let i=0;i<window.frames.length;i++) {
                 CurrentDocument = window.frames[i].document
                 CurrentDocument.addEventListener('scroll',getLink.bind(null, CurrentDocument));
+                CurrentDocument.addEventListener('load',getLink.bind(null, CurrentDocument));
             }
         }
-    }
+    })
 
 })();
