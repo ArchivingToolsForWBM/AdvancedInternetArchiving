b3ab3007ceeeb3a7839aba1b65ee9d2064e62947
GhbSmwc <89951208+ArchivingToolsForWBM@users.noreply.github.com>
2022-08-13 01:04:21 -0400

    fix some several issues
    Checks if the string is even valid before placing it on the console.log
    
    Credit to Mark Hewitt on this post: https://stackoverflow.com/questions/14013131/how-to-get-background-image-url-of-an-element-using-javascript for a sure fire way to obtain the URL from "url(\"<link>")" in a style attribute on background images.

--- GreaseMonkey_GeneralLinks.js
+++ GreaseMonkey_GeneralLinks.js
@@ -7,38 +7,53 @@
 // @grant        none
 // ==/UserScript==
 
-
+//Note: This was tested on firefox.
 (function() {
 	'use strict';
 	const all = window.allLink = new Set();
-	
 	function getLink(PageDocument) {
 		Array.from(PageDocument.getElementsByTagName('a')).forEach(link=>{ //"a href" links
-			let URLString = link.href
-			if(!all.has(URLString)) {
-				all.add(URLString);
-				console.log((URLString).replace(/^http/, "ttp").replace(/#.*$/, ""));
+			let URLString = FormatURL(link.href)
+			if(!all.has(URLString[0])&&URLString[1]) {
+				all.add(URLString[0]);
+				console.log((URLString[0]).replace(/^http/, "ttp").replace(/#.*$/, ""));
 			}
 		});
 		Array.from(PageDocument.getElementsByTagName('img')).forEach(link=>{ //Images
-			let URLString = link.src
-			if(!all.has(URLString)) {
-				all.add(URLString);
-				console.log((URLString).replace(/^http/, "ttp").replace(/#.*$/, ""));
+			let URLString = FormatURL(link.src)
+			if(!all.has(URLString[0])&&URLString[1]) {
+				all.add(URLString[0]);
+				console.log((URLString[0]).replace(/^http/, "ttp").replace(/#.*$/, ""));
 			}
 		});
 		Array.from(PageDocument.getElementsByTagName('*')).forEach(link=>{ //Background images
-			let URLString = link.style.backgroundImage
-			if(!all.has(URLString)&&URLString!="") {
-				all.add(URLString);
-				console.log((URLString).replace(/url\((\"|\')/, "").replace(/(\"|\')\)/, "").replace(/^http/, "ttp"));
+			let URLString = FormatURL(link.style.backgroundImage.slice(5, -2))
+			if(!all.has(URLString[0])&&URLString[1]) {
+				all.add(URLString[0]);
+				console.log((URLString[0]).replace(/^http/, "ttp"));
 			}
 		});
 	}
+	
+	function FormatURL(String) {
+		let IsStringValid = true
+		if (String=="javascript:void(0);"||String=="none"||String=="") {
+			IsStringValid = false
+		}
+		if (IsStringValid) {
+			if (/^\/+/.test(String)) {
+				String = String.replace(/^\/+/, "https://")
+			} else if (/^(?!http(s)?:\/\/)/.test(String)) {
+				String = String.replace(/^/, "https://")
+			}
+		}
+		return [String, IsStringValid]
+	};
+	
 	//Code that executes when the MAIN WINDOW loads the page
 	//Please note that this does not reflect the loading of subwindows when you open links in a way that does not reload the main window
 	//Since this executes ONCE when the main window loads.
-  
+
 	window.addEventListener('load',getLink.bind(null, document)); //Get links on the main window when page finishes loading
 	window.addEventListener('load', (event) => {
 		let CurrentDocument = document
