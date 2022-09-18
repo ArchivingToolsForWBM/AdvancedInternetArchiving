ae52e56f945f92d7ff77762df1f6186a19f6c774
GhbSmwc <89951208+ArchivingToolsForWBM@users.noreply.github.com>
2022-09-18 00:56:33 -0400

    working on a new script - Greasemonkey_NitterAutoNext
    WIP though, breaks on searches, this basically "autoclicks" the "Load more" button so you can AFK while the other scripts are extracting links. Be careful though, even with auto-refreshes this may not extract every tweet, despite nitter, the front end of twitter, not use twitter's API, which that encumbered by the 7-day not show some tweets and other on-twitter restrictions.

---
+++ GreasemonkeyScripts/Greasemonkey_NitterAutoNext.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name          Nitter - Auto next page
+// @version       1
+// @description   Automatically clicks on "Load more", for AFK extracting links
+// @include       https://nitter.net/*
+// @grant         none
+// ==/UserScript==
+
+
+(function(){
+	window.addEventListener('load', LoadURLAfterTimer)
+	
+	function LoadURLAfterTimer() {
+		setTimeout(ClickLoadMore, 500)
+	}
+	
+	function ClickLoadMore() {
+		let IndexOfClassThatIsLoadMore = -1
+		for (let i = 0; i < document.getElementsByClassName("show-more").length && IndexOfClassThatIsLoadMore == -1; i++) {
+			if (/\<a href=\".+\"\>Load more/.test(document.getElementsByClassName("show-more")[i].innerHTML)) {
+				IndexOfClassThatIsLoadMore = i
+			}
+		}
+		if (IndexOfClassThatIsLoadMore != -1) {
+			let UsernamePartURL = window.location.href.replace(/\?cursor=.*$/, "")
+			let LoadMoreButton = document.getElementsByClassName("show-more")[IndexOfClassThatIsLoadMore].innerHTML.replace("<a href=\"", "").replace(/\".*$/, "")
+			location.href = UsernamePartURL + LoadMoreButton
+		}
+		if (document.getElementsByClassName("timeline-end").length != 0) { //Nitter tends to end up "No More Items" when there are actually more tweets, when this happens, reload
+			let FoundNoMoreItems = false
+			for (let i = 0; i < document.getElementsByClassName("timeline-end").length && FoundNoMoreItems == false; i++) {
+				if (document.getElementsByClassName("timeline-end")[i].innerHTML == "No more items") {
+					FoundNoMoreItems = true
+				}
+			}
+			if (FoundNoMoreItems) {
+				location.reload()
+			}
+		}
+	}
+})();
