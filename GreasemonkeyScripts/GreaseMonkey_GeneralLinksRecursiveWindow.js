aa8bfd829b139a455657a70e14d966eb395bc935
GhbSmwc <89951208+ArchivingToolsForWBM@users.noreply.github.com>
2023-01-20 18:21:33 -0500

    update
    Fixed a bug that resulted in logging text that aren't URLs from CSS/style background images (such as linear graidents).

--- GreasemonkeyScripts/GreaseMonkey_GeneralLinksRecursiveWindow.js
+++ GreasemonkeyScripts/GreaseMonkey_GeneralLinksRecursiveWindow.js
@@ -61,7 +61,7 @@
 		});
 	}
 	function AddLinksToSetAndConsoleLog(String_URL, ObtainedFrom) {
-		if (typeof String_URL == "string") { //failsafe
+		if (isValidURL(String_URL)) {
 			if (!((/^\s*javascript:.*$/.test(String_URL))||String_URL=="none"||String_URL=="")){ //Accept only valid URLs
 				//If URLs starts with a slash, prepend it with a "//"
 					let CorrectedURL = String_URL.replace(/#.*$/, "") //get rid of fragment identifier (it's technically not an address), also prevents same URLs with different fragments from filling up the set.
@@ -99,8 +99,20 @@
 			IframeRecursionCount-- //Don't count as total number of function calls, just how many levels deep.
 		}
 	}
-	
-	
+	function isValidURL(str) { //Credit: https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+		var pattern = new RegExp('^((https?:)?\\/\\/)?'+ // protocol
+			'(?:\\S+(?::\\S*)?@)?' + // authentication
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+			'(\\#[-a-z\\d_]*)?$','i'); // fragment locater
+		if (!pattern.test(str)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 	window.addEventListener('scroll',addEventListenersToPages.bind(null, window));
 	window.addEventListener('load',addEventListenersToPages.bind(null, window));
 	if (Interval_captureLinks) {
