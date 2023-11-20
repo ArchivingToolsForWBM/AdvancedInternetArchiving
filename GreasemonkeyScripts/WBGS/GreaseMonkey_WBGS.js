4f7e33764974c0da6f7c967660f0370b5e008424
? <89951208+ArchivingToolsForWBM@users.noreply.github.com>
2023-11-20 18:36:44 -0500

    the duplicating bug is annoying as f*ck if you don't have the "Save results in a new Sheet" checked
    As said in the checker tool, there is a bug that causes processes to duplicate, overwrite save results. We don't want that as it basically destroys information from the first process.

---
+++ GreasemonkeyScripts/WBGS/GreaseMonkey_WBGS.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         WBGS - Auto-check "Save results in a new Sheet."
+// @namespace    WBGS_autocheck
+// @version      0.1
+// @description  So duped processes don't overwrite save results.
+// @include      https://archive.org/services/wayback-gsheets/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	setInterval(Code, 100)
+	//^Had to be an interval instead of a timeout because when you click on links to another WBGS page, that isn't a page refresh
+	// rather a dynamic page (https://en.wikipedia.org/wiki/Dynamic_web_page ) similar to being on twitter and clicking on links
+	// to another twitter page, which result in this JS code not executing until you refresh the page.
+	
+	function Code() {
+		if (window.location.href == "https://archive.org/services/wayback-gsheets/check?method=archive") {
+			document.querySelectorAll('input[type=checkbox]')[2].checked = true //Check the 3rd option, "Save results in a new Sheet."
+		}
+	}
+})();
