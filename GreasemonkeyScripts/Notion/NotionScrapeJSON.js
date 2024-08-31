fd07b310d1f884cacdfb7aadbc09aab26167da18
? <89951208+ArchivingToolsForWBM@users.noreply.github.com>
2024-08-30 23:03:40 -0400

    WIP

--- GreasemonkeyScripts/Notion/NotionScrapeJSON.js
+++ GreasemonkeyScripts/Notion/NotionScrapeJSON.js
@@ -11,18 +11,25 @@
 // ==/UserScript==
 
 (() => {
-	let SavedData = []
+	//Initialize some values
+		let SavedData = {
+			Settings: {
+				OnOffState: false
+			},
+			ScrapedContent: []
+		}
+		LoadSavedValues()
+	//Don't touch
+		let RegExpPreset_BlankOrJustSpaces = /^$[\s\u200B]*/
+		let RegExpPreset_ClassNotionBlockName = /notion-(?:text|image|column_list|(?:sub_)+header|toggle)-block/
+		
+		setInterval(RevealAll, 2000)
+		setTimeout(SpawnUI, 2000)
+		setTimeout(ExtractPageContent, 4000)
+	//Elements to remember (best not to touch)
+		let UI_Button_OnOff = null
+		let UI_TextDisplay_ScrapedPageCount = null
 	
-	let RegExpPreset_BlankOrJustSpaces = /^$[\s\u200B]*/
-	let RegExpPreset_ClassNotionBlockName = /notion-(?:text|image|column_list|(?:sub_)+header|toggle)-block/
-	
-	setInterval(RevealAll, 2000)
-	setTimeout(ExtractPageContent, 4000)
-	
-	let Object_SvgPathsToName = {
-		"M1.5,1.5 L1.5,14.5 L14.5,14.5 L14.5,1.5 L1.5,1.5 Z M0,0 L16,0 L16,16 L0,16 L0,0 Z": false, //A square box indicating uncheck checkbox
-		"5.5 11.9993304 14 3.49933039 12.5 2 5.5 8.99933039 1.5 4.9968652 0 6.49933039": true, //Checked checkbox
-	}
 	
 	function RevealAll() {
 		//This finds all the "chevron" or any buttons that reveal content without opening an entire new page
@@ -88,10 +95,98 @@
 		})
 		ShowDescriptionButtons.forEach(ele => ele.click())
 	}
-	
+	function SpawnUI() {
+		let DivBox = document.createElement("div")
+		//BoxOfUI.setAttribute("style", "position: fixed;bottom: 40px;right: 40px;z-index: 999; background-color: rgba(64, 64, 64, .5); color: #ffffff; border-radius: 30px; padding: 15px;")
+		DivBox.style.position = "fixed"
+		DivBox.style.bottom = "40px"
+		DivBox.style.right = "40px"
+		DivBox.style.zIndex = "999"
+		DivBox.style.backgroundColor = "rgba(64, 64, 64, .5)"
+		DivBox.style.color = "#ffffff"
+		DivBox.style.borderRadius = "30px"
+		DivBox.style.padding = "15px"
+		
+		
+		try {
+			let DivBoxToPlaceIn = document.querySelector("body")
+			DivBoxToPlaceIn.appendChild(DivBox)
+		} catch (e) {
+			window.alert("Failed to place notion scrape UI")
+		}
+		//On off button
+			UI_Button_OnOff = document.createElement("button")
+			UI_Button_OnOff.textContent = "Start"
+			if (SavedData.Settings.OnOffState) {
+				UI_Button_OnOff.textContent = "Stop"
+			}
+			UI_Button_OnOff.style.width = "50px"
+			UI_Button_OnOff.style.color = "#000000"
+			UI_Button_OnOff.addEventListener(
+				"click",
+				function () {
+					SavedData.Settings.OnOffState = !SavedData.Settings.OnOffState
+					this.textContent = "Start"
+					if (SavedData.Settings.OnOffState) {
+						this.textContent = "Stop"
+					}
+					SaveSavedValues()
+				}
+			)
+			DivBox.appendChild(UI_Button_OnOff)
+		//Copy scraped data
+			let UI_CopyScrapeJson = document.createElement("button")
+			UI_CopyScrapeJson.textContent = "Copy Scraped data"
+			UI_CopyScrapeJson.style.color = "#000000"
+			UI_CopyScrapeJson.addEventListener(
+				"click",
+				function () {
+					GM.setClipboard(JSON.stringify(SavedData.ScrapedContent, null, " "))
+				}
+			)
+			DivBox.appendChild(UI_CopyScrapeJson)
+		//Clear scraped data
+			let UI_ClearScrapedData = document.createElement("button")
+			UI_ClearScrapedData.textContent = "Clear Scraped data"
+			UI_ClearScrapedData.style.color = "#000000"
+			UI_ClearScrapedData.addEventListener(
+				"click",
+				function () {
+					if (window.confirm("Are you sure you want to clear it?")) {
+						SavedData.ScrapedContent = []
+						SaveSavedValues()
+					}
+				}
+			)
+			DivBox.appendChild(UI_ClearScrapedData)
+			DivBox.appendChild(document.createElement("br"))
+		//Statistics
+			let StatisticsTable = document.createElement("table")
+			let StatisticsTable_Row_ScrapePageCount = document.createElement("tr")
+			StatisticsTable.appendChild(StatisticsTable_Row_ScrapePageCount)
+			
+			let StatisticsTable_Cell_ScrapeCountKey = document.createElement("td")
+			StatisticsTable_Cell_ScrapeCountKey.style.fontFamily = "monospace"
+			StatisticsTable_Cell_ScrapeCountKey.appendChild(document.createTextNode("Number of scraped pages: "))
+			StatisticsTable_Cell_ScrapeCountKey.style.padding = "5px"
+			StatisticsTable_Row_ScrapePageCount.appendChild(StatisticsTable_Cell_ScrapeCountKey)
+			
+			let StatisticsTable_Cell_ScrapeCountValue = document.createElement("td")
+			StatisticsTable_Cell_ScrapeCountValue.style.fontFamily = "monospace"
+			UI_TextDisplay_ScrapedPageCount = StatisticsTable_Cell_ScrapeCountValue
+			StatisticsTable_Cell_ScrapeCountValue.appendChild(document.createTextNode("0"))
+			StatisticsTable_Cell_ScrapeCountValue.style.padding = "5px"
+			StatisticsTable_Row_ScrapePageCount.appendChild(StatisticsTable_Cell_ScrapeCountValue)
+			
+			UI_TextDisplay_ScrapedPageCount.textContent = SavedData.ScrapedContent.length.toString(10)
+			
+			DivBox.appendChild(StatisticsTable)
+			
+			UpdateUIInfoDisplay()
+	}
 	function ExtractPageContent() {
-		let CollectedDataObject = {
-			PageURL: location.href,
+		let CollectedDataOfPage = {
+			PageURL: location.href.replace(/\?pvs=\d*$/),
 			PageContent: []
 		}
 		let ReferenceNode = document.querySelector(".layout-content") //Start at the title header
@@ -109,16 +204,22 @@
 			
 			Contents.forEach((ContentThing, Index) => {
 				let Data = GetContentFromSection(ContentThing, Index)
-				CollectedDataObject.PageContent.push(Data)
+				CollectedDataOfPage.PageContent.push(Data)
 			});
 			
+			let DuplicateItem = SavedData.ScrapedContent.find(PageOfContent => { //Find duplicates
+				return CollectedDataOfPage.PageURL == PageOfContent.PageURL
+			})
 			
+			if (typeof DuplicateItem == "undefined") { //If no dupes found, insert it.
+				SavedData.ScrapedContent.push(CollectedDataOfPage)
+				SaveSavedValues()
+			}
+
 		} else { //"gallery" page
 			
 			
 		}
-		
-		GM.setClipboard(JSON.stringify(CollectedDataObject, null, " "))
 	}
 	
 	function GetContentFromSection(node, Index) {
@@ -172,6 +273,11 @@
 					try {
 						OutputTableRows.TableValue.HTMLCode = Cells[1].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].innerHTML ?? "?" //SVG, background images, images, so innerHTML works here.
 					} catch {}
+					let Links = ExtractLinks(Cells[1])
+					if (Links.length != 0) {
+						OutputTableRows.Links = Links
+					}
+					
 					return OutputTableRows
 				})
 				OutputObject.Contents.push(OutputTableObjectFormatted)
@@ -218,17 +324,6 @@
 		//Done
 			return OutputObject
 	}
-	
-	
-	async function LoadSavedValues() {
-		try {
-			SavedData = await GM.getValue("Notion_Scrape_SavedData", "[]")
-		} catch (e) {
-			alert(e)
-		}
-		
-	}
-	
 	function GetListSubBlocks(node) {
 		let ListOfBlocks = [...node.querySelectorAll(".notion-image-block,notion-text-block")]
 		OutputList = ListOfBlocks.map(Block => {
@@ -260,28 +355,6 @@
 		})
 		return OutputList
 	}
-	function GetToggleBlockData(node) {
-		let Output = {Type: "Toggle"}
-		let ListOfDivs = [...node.childNodes]
-		let OutputToggleList = ListOfDivs.map((ToggleBlock, Index) => {
-			if ((!RegExpPreset_BlankOrJustSpaces.test(ToggleBlock.innerText)) && Index == 0) {
-				let Text = ToggleBlock.innerText
-				return {
-					Title: ToggleBlock.innerText
-				}
-			} else {
-				try {
-					let NotionBlocksOfToggle = GetNotionBlocks(ToggleBlock.childNodes[0])
-					return NotionBlocksOfToggle
-				} catch (e) {
-					return "Error! Unknown format"
-				}
-			}
-			
-		})
-		Output.ToggleItems = OutputToggleList
-		return Output
-	}
 	function GetNotionBlocks(node) {
 		let BlocksArray = [...node.childNodes]
 		let OutputObjectArr = BlocksArray.map(Block => {
@@ -309,6 +382,48 @@
 			
 		})
 		return OutputObjectArr
+	}
+	//Saved and Statistics
+		async function LoadSavedValues() {
+			try {
+				SavedData = JSON.parse(await GM.getValue("Notion_Scrape_SavedData", JSON.stringify(SavedData)))
+			} catch (e) {
+				window.alert("Issue with Loading data. " + e)
+			}
+			
+		}
+		async function SaveSavedValues() {
+			try {
+				await GM.setValue("Notion_Scrape_SavedData", JSON.stringify(SavedData))
+				UpdateUIInfoDisplay()
+			} catch (e) {
+				window.alert("Issue with Saving data. " + e)
+			}
+		}
+		function UpdateUIInfoDisplay() {
+			UI_TextDisplay_ScrapedPageCount.textContent = SavedData.ScrapedContent.length.toString(10)
+		}
+	function GetToggleBlockData(node) {
+		let Output = {Type: "Toggle"}
+		let ListOfDivs = [...node.childNodes]
+		let OutputToggleList = ListOfDivs.map((ToggleBlock, Index) => {
+			if ((!RegExpPreset_BlankOrJustSpaces.test(ToggleBlock.innerText)) && Index == 0) {
+				let Text = ToggleBlock.innerText
+				return {
+					Title: ToggleBlock.innerText
+				}
+			} else {
+				try {
+					let NotionBlocksOfToggle = GetNotionBlocks(ToggleBlock.childNodes[0])
+					return NotionBlocksOfToggle
+				} catch (e) {
+					return "Error! Unknown format"
+				}
+			}
+			
+		})
+		Output.ToggleItems = OutputToggleList
+		return Output
 	}
 	//Helper
 		function getElementsByText(ObjReference, RegexText, Tag) {
